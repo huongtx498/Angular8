b0db164c3707bdac4baf3925dfc53c05fff83892
trinhhuong98765 <trinhhuong244@gmail.com>
2020-04-19 16:27:38 +0700

    get start with component

--- Angular_test/ng-codes/src/app/app.component.ts
+++ Angular_test/ng-codes/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-codes';
+  name = 'Trình Hương';
+  Border = true;
+  message = 'Click me!';
+  method1() {
+    this.Border = !this.Border;
+    this.message = this.Border ? 'Click me!' : 'No, stop!';
+  }
 }
