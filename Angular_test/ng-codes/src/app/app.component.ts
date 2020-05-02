d45d62fdb27c132d33a2ea6afe90da711c45f0a7
trinhhuong244 <trinhhuong244@gmail.com>
2020-05-02 10:03:08 +0700

    onInit event

--- Angular_test/ng-codes/src/app/app.component.ts
+++ Angular_test/ng-codes/src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {}
   title = 'ng-codes';
   name = 'Trình Hương';
   Border = true;
   message = 'Click me!';
+  ngOnInit() {}
   method1() {
     this.Border = !this.Border;
     this.message = this.Border ? 'Click me!' : 'No, stop!';
